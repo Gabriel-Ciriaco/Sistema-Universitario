70c7459036adb1d5f21f9c2bf8116aa9e55878dd
Gasegamer <gabrielciricarvalho@gmail.com>
2025-05-12 11:49:36 -0300

    Adicionado Departamentos na Universidade.
    A classe Universidade conta agora com a opção de adicionar, remover e retornar um vetor de departamentos.

--- src/com/universidade/Universidade.java
+++ src/com/universidade/Universidade.java
@@ -1,11 +1,16 @@
 package com.universidade;
 
+import com.departamento.Departamento;
+
+
 public class Universidade {
   private String nome;
+  private Departamento[] departamentos;
 
-  public Universidade(String nome)
+  public Universidade(String nome, int numDepartamentos)
   {
     this.nome = nome;
+    this.departamentos = new Departamento[numDepartamentos];
   }
 
   public String getNome()
@@ -16,5 +21,50 @@ public class Universidade {
   public void setNome(String nome)
   {
     this.nome = nome;
+  }
+
+  public Departamento[] getDepartamentos()
+  {
+    return this.departamentos;
+  }
+
+  public boolean addDepartamento(Departamento departamento)
+  {
+    int i;
+
+    for (i = 0; i < this.departamentos.length; i++)
+    {
+      if (this.departamentos[i] == null)
+      {
+        this.departamentos[i] = departamento;
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public boolean removeDepartamento(String codigoDepartamento)
+  {
+    for (int i = 0; i < this.departamentos.length; i++)
+    {
+      if (this.departamentos[i] != null &&
+          this.departamentos[i].getCodigo() == codigoDepartamento)
+        {
+          int j;
+
+          for (j = i; j + 1 < this.departamentos.length; j++)
+          {
+            this.departamentos[j] = this.departamentos[j + 1];
+          }
+
+          this.departamentos[j] = null;
+
+          return true;
+        }
+    }
+
+    return false;
   }
 }
