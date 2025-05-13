7884c61b48a64f83f2440749ea864faa9a5d8c3e
Carol-Nunes <cacanunesaraujo@gmail.com>
2025-05-13 11:25:03 -0300

    Update Docente
    Docente class updated

--- src/com/funcionario/docente/Docente.java
+++ src/com/funcionario/docente/Docente.java
@@ -3,6 +3,28 @@ package com.funcionario.docente;
 import com.funcionario.Funcionario;
 
 
-public class Docente extends Funcionario {
+public abstract class Docente extends Funcionario {
+
+    String titulacao;
+
     
+    public Docente(String codigo, String nome, double salario, String nivel, String titulacao) 
+    {
+        super(codigo, nome, salario, nivel);
+        this.titulacao = titulacao;
+    }
+
+    public String getTitulacao() 
+    {
+        return titulacao;
+    }
+
+    public void setTitulacao(String titulacao) 
+    {
+        this.titulacao = titulacao;
+    }
+
+    public abstract double calcularSalario();
+
+
 }
