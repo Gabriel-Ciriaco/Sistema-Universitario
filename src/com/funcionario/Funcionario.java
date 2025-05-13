4c922751ccdd33b95df5a091532bd5bc53d5edcd
Carol-Nunes <cacanunesaraujo@gmail.com>
2025-05-13 11:20:23 -0300

    Update Funcionario
    Funcionario class updated

--- src/com/funcionario/Funcionario.java
+++ src/com/funcionario/Funcionario.java
@@ -1,7 +1,51 @@
 package com.funcionario;
 
-public class Funcionario {
+public abstract class Funcionario {
+
   private String codigo;
+  private String nome;
+  private double salario;
+  private String nivel;
+
+  
+
+  public Funcionario(String codigo, String nome, double salario, String nivel) 
+  {
+    this.codigo = codigo;
+    this.nome = nome;
+    this.salario = salario;
+    this.nivel = nivel;
+  }
+
+  public String getNome() 
+  {
+    return nome;
+  }
+
+  public void setNome(String nome) 
+  {
+    this.nome = nome;
+  }
+
+  public double getSalario() 
+  {
+    return salario;
+  }
+
+  public void setSalario(double salario) 
+  {
+    this.salario = salario;
+  }
+
+  public String getNivel() 
+  {
+    return nivel;
+  }
+
+  public void setNivel(String nivel) 
+  {
+    this.nivel = nivel;
+  }
 
   public String getCodigo()
   {
@@ -12,4 +56,8 @@ public class Funcionario {
   {
     this.codigo = codigo;
   }
+
+  public abstract double calcularSalario();
+
+
 }
