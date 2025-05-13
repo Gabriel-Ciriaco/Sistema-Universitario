a050dc989b9e900b069de645027f135f0c6c42f5
Carol-Nunes <cacanunesaraujo@gmail.com>
2025-05-13 11:43:15 -0300

    Update Efetivo
    Efetivo class updated

--- src/com/funcionario/docente/Efetivo.java
+++ src/com/funcionario/docente/Efetivo.java
@@ -1,5 +1,44 @@
 package com.funcionario.docente;
 
 public class Efetivo extends Docente {
-    
+
+    String area;
+
+    public Efetivo(String codigo, String nome, double salario, String nivel, String titulacao, String area) 
+    {
+        super(codigo, nome, salario, nivel, titulacao);
+        this.area = area;
+    }
+
+    public String getArea() 
+    {
+        return area;
+    }
+
+    public void setArea(String area) 
+    {
+        this.area = area;
+    }
+
+    public double calcularSalario()
+    {
+        double adicional;
+
+        if(getNivel().equals("D1"))
+        {
+            adicional = 0.05;
+        
+        }else if(getNivel().equals("D2")){
+
+            adicional = 0.1;
+
+        }else{
+
+            adicional = 0.2;
+        }
+
+        return getSalario() + (getSalario() * adicional);
+
+    }
+
 }
