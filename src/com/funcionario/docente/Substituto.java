b2523a043b37eca9b791c20a76156f5eae7f7bf7
Carol-Nunes <cacanunesaraujo@gmail.com>
2025-05-13 11:38:14 -0300

    Update Substituto
    Substituto class updated

--- src/com/funcionario/docente/Substituto.java
+++ src/com/funcionario/docente/Substituto.java
@@ -2,5 +2,41 @@ package com.funcionario.docente;
 
 
 public class Substituto extends Docente {
+
+    int cargaHoraria;
+
+    public Substituto(String codigo, String nome, double salario, String nivel, String titulacao, int cargaHoraria) 
+    {
+        super(codigo, nome, salario, nivel, titulacao);
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public int getCargaHoraria() 
+    {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) 
+    {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public double calcularSalario()
+    {
+        double adicional;
+
+        if(getNivel().equals("S1"))
+        {
+            adicional = 0.05;
+        
+        }else{
+
+            adicional = 0.1;
+
+        }
+
+        return getSalario() + (getSalario() * adicional);
+    }
+
     
 }
